abd3b0e88f62ca9aa2516d864ecc56e69e123589
Kyle <kyle@chilcutt.com>
2013-05-15 23:14:48 -0700

    Add Floor#initialize spec
    Floors should know their own number.  The number should be readible
    publicly.

--- fandor/spec/models/floor_spec.rb
+++ fandor/spec/models/floor_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../spec_helper.rb'
 
 describe Floor do
+  describe '#initialize' do
+    it 'assigns a floor number from argument' do
+      Floor.new(1).number.should == 1
+      Floor.new(100).number.should == 100
+    end
+  end
 end
