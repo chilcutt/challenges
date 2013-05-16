04e97bc52266e68ce135bbaaee55a9118a17ccc7
Kyle <kyle@chilcutt.com>
2013-05-15 23:53:14 -0700

    Add Elevator#move spec
    Move should make the current_floor into the new floor.

--- fandor/spec/models/elevator_spec.rb
+++ fandor/spec/models/elevator_spec.rb
@@ -3,6 +3,7 @@ require_relative '../spec_helper.rb'
 describe Elevator do
   let(:building) { double('building') }
   let(:floor) { double('floor') }
+  let(:other_floor) { double('other_floor') }
   let(:elevator) { Elevator.new(building, floor) }
 
   describe '#initialize' do
@@ -12,6 +13,14 @@ describe Elevator do
 
     it 'has a starting floor' do
       elevator.current_floor.should == floor
+    end
+  end
+
+  describe '#move' do
+    it 'changes the current floor' do
+      elevator.current_floor.should == floor
+      elevator.move(other_floor)
+      elevator.current_floor.should == other_floor
     end
   end
 end
