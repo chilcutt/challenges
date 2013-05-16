05d79ce02c0d6f7f3ab6c65418f1bb9ad402167b
Kyle <kyle@chilcutt.com>
2013-05-15 23:10:58 -0700

    Add Building#enter spec
    When a user calls #enter on a Building object, it should return the
    first floor.  Dry up the spec by moving definitions and stubs outside of
    the method blocks and make the first floor into a different double in
    order to test the #enter method.

--- fandor/spec/models/building_spec.rb
+++ fandor/spec/models/building_spec.rb
@@ -1,20 +1,29 @@
 require_relative '../spec_helper'
 
 describe Building do
+  let(:building) { Building.new(5) }
+  let(:elevator) { double('elevator') }
+  let(:first_floor) { double('first_floor') }
+  let(:other_floor) { double('other_floor') }
+
+  before(:each) do
+    Floor.stub(:new).and_return(first_floor, other_floor)
+    Elevator.stub(:new => elevator)
+  end
+
   describe '#initialize' do
-    let(:elevator) { double('elevator') }
-
-    before(:each) do
-      Floor.stub(:new => double('floor'))
-      Elevator.stub(:new => elevator)
-    end
-
     it 'creates a building with the number of floors from arguments' do
-      Building.new(5).floors.size.should == 5
+      building.floors.size.should == 5
     end
 
     it 'creates a building with a single elevator' do
-      Building.new(1).elevator.should == elevator
+      building.elevator.should == elevator
+    end
+  end
+
+  describe '#enter' do
+    it 'returns the first floor' do
+      building.enter.should == first_floor
     end
   end
 end
