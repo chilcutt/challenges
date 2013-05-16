8831744aa6e3bc4901e26c246da72ed2e7547539
Kyle <kyle@chilcutt.com>
2013-05-15 23:01:47 -0700

    Add #initialize spec for Building class
    Initializing a Building should create a Building with the required
    number of Floors and a single Elevator.  Stub out the other models until
    we create them.

--- fandor/spec/models/building_spec.rb
+++ fandor/spec/models/building_spec.rb
@@ -1,4 +1,20 @@
 require_relative '../spec_helper'
 
 describe Building do
+  describe '#initialize' do
+    let(:elevator) { double('elevator') }
+
+    before(:each) do
+      Floor.stub(:new => double('floor'))
+      Elevator.stub(:new => elevator)
+    end
+
+    it 'creates a building with the number of floors from arguments' do
+      Building.new(5).floors.size.should == 5
+    end
+
+    it 'creates a building with a single elevator' do
+      Building.new(1).elevator.should == elevator
+    end
+  end
 end
