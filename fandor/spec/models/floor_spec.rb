6f7eb34ef174177c4a7ae5c69d7eb2f0940e5e0b
Kyle <kyle@chilcutt.com>
2013-05-15 23:20:10 -0700

    Floors belong_to a building
    A floor needs a pointer to the building it belongs to.  In Rails, this
    would be a belong_to relation with a foreign key on the Buildings'
    table.

--- fandor/spec/models/floor_spec.rb
+++ fandor/spec/models/floor_spec.rb
@@ -1,10 +1,16 @@
 require_relative '../spec_helper.rb'
 
 describe Floor do
+  let(:building) { double('building') }
+
   describe '#initialize' do
     it 'assigns a floor number from argument' do
-      Floor.new(1).number.should == 1
-      Floor.new(100).number.should == 100
+      Floor.new(building, 1).number.should == 1
+      Floor.new(building, 100).number.should == 100
+    end
+
+    it 'belongs to a building' do
+      Floor.new(building, 1).building.should == building
     end
   end
 end
