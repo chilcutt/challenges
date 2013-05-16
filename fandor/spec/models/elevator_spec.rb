b595599706c35142808409be57ba6580da940946
Kyle <kyle@chilcutt.com>
2013-05-15 23:36:29 -0700

    Add Elevator#initialize spec
    Elevator belongs to a Building and has a current Floor.  In
    ActiveRecord, these would be belong_to relations.

--- fandor/spec/models/elevator_spec.rb
+++ fandor/spec/models/elevator_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../spec_helper.rb'
 
 describe Elevator do
+  let(:building) { double('building') }
+  let(:floor) { double('floor') }
+  let(:elevator) { Elevator.new(building, floor) }
+
+  describe '#initialize' do
+    it 'belongs to the building' do
+      elevator.building.should == building
+    end
+
+    it 'has a starting floor' do
+      elevator.floor.should == floor
+    end
+  end
 end
